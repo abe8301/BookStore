f68848fab24d4ae1d99f63389900c3d75d4f7750
Arkadiusz B <32898561+abe8301@users.noreply.github.com>
2017-12-22 20:12:08 +0100

    test1

---
+++ BookStore/Migrations/201712221713549_InitialModel.cs
@@ -0,0 +1,54 @@
+namespace BookStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsSubscribedToNewsletter = c.Boolean(nullable: false),
+                        MembershipTypeId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.MembershipTypes", t => t.MembershipTypeId, cascadeDelete: true)
+                .Index(t => t.MembershipTypeId);
+            
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SingUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropTable("dbo.MembershipTypes");
+            DropTable("dbo.Customers");
+            DropTable("dbo.Books");
+        }
+    }
+}
---
+++ BookStore/Models/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+public class MyDbContext : DbContext
+{
+    
+    
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<MembershipType> MembershipType { get; set; }
+
+
+}
